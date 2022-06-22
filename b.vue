affc91ae0cde31a82f8b599abf4d69a8f76f4399
linshimao <931520261@qq.com>
2022-06-22 10:23:53 +0800

    Create b.vue

---
+++ b.vue
@@ -0,0 +1,3 @@
+const e = encodeURIComponent(`lessonId=${123}`) // const e = `lessonId=${123}`
+Taro.navigateTo({ url: `/pages/record-detail/record-detail?${e}` }) //
+目标页打印 const { router } = getCurrentInstance() console.log(router)
