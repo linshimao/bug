545a4eef4172336129bcceb6b38ee308b035367c
linshimao <931520261@qq.com>
2022-06-22 10:25:34 +0800

    Update b.vue

--- b.vue
+++ b.vue
@@ -1,3 +1,10 @@
-const e = encodeURIComponent(`lessonId=${123}`) // const e = `lessonId=${123}`
-Taro.navigateTo({ url: `/pages/record-detail/record-detail?${e}` }) //
-目标页打印 const { router } = getCurrentInstance() console.log(router)
+<template></template>
+
+<script setup>
+const e = encodeURIComponent(`lessonId=${123}`);
+// const e = `lessonId=${123}` // 如果不转义，就正常
+Taro.navigateTo({ url: `/pages/record-detail/record-detail?${e}` });
+// 目标页打印
+const { router } = getCurrentInstance();
+console.log(router);
+</script>
